dcb560e0a69bce0f92f03bda70f4e2a4d8c29404
Erik Almaraz <erikalmaraz@fastmail.com>
2024-03-08 21:18:01 -0800

    Add first 'dummy' ccl lisp file.

---
+++ chembook-core.lisp
@@ -0,0 +1,16 @@
+;; Copyright 2024 - Erik P. Almaraz
+;;
+;; This file is part of ChemBook.
+
+;; Commentary:
+;;
+;; This file constituate the core features of ChemBook.
+;;
+
+;; Code:
+
+(defun hello-world (name)
+  "Hello world from `name'."
+  (format t "Hello World!~& - from: ~a ~&" name))
+
+(hello-world "ChemBook")
