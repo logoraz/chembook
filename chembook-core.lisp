2c943753777a6060edd66f1d0ff6d861c031fbaa
Erik Almaraz <erikalmaraz@fastmail.com>
2024-03-08 22:15:39 -0800

    Add first function and Macro's - may the hacking begin!

--- chembook-core.lisp
+++ chembook-core.lisp
@@ -14,8 +14,18 @@
 
 ;; Code:
 
+;; First Function
 (defun hello-world (name)
   "Hello world from `name'."
   (format t "Hello World!~& - from: ~a ~&" name))
 
 (hello-world "ChemBook")
+
+;; First Macro's - An important step for Lisp!
+(defmacro incr (var)
+  "Macro to increment var."
+  `(setq ,var (1+ ,var)))
+
+(defmacro decr (var)
+  "Macro to decrement var."
+  `(setq ,var (1- ,var)))
